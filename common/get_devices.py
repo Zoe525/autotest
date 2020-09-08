2e5c73f942323a0f4f083eb33cd9d7311a462f9a
unknown <13811739565@adc.com>
2020-09-08 11:36:24 +0800

    获取设备

---
+++ common/get_devices.py
@@ -0,0 +1,25 @@
+import time
+from appium import webdriver
+
+des={
+    'platformName': 'Android',
+    'platformVersion': '10', #填写android的系统版本
+    'deviceName': 'oppo reno', #填写设备名称
+    'appPackage': 'com.android.mms', #填写被测试包名
+    'appActivity': '.ui.ConversationList', #填写被测试app入口
+    'udid': '88ccb924', # 填写通过命令行 adb devices 查看到的 uuid
+    'noReset': True,
+    'unicodeKeyboard': True,
+    'resetKeyboard': True,
+}
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', des)
+
+# driver.find_element_by_xpath('//android.widget.TextView[@text="消息"]').click()
+# time.sleep(1)
+driver.find_element_by_id('com.android.mms:id/action_start_new_conversation').click()
+time.sleep(1)
+driver.find_element_by_id('com.android.mms:id/recipient_text_view').send_keys('13811346164')
+driver.keyevent(66)
+time.sleep(1)
+
+
