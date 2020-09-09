d7cddbbc4861418ab1b8be5a2e7715531ce61e7f
Zoe <550721827@qq.com>
2020-09-09 19:38:17 +0800

    封装发送过程 加判断

--- common/send_messaging.py
+++ common/send_messaging.py
@@ -3,14 +3,32 @@ import time
 from get_devices import driver
 
 
-# def send_messaging(content):
-#     time.sleep(1)
-   driver.find_element_by_id("com.android.mms:id/compose_message_text").send_keys("文本")
+def send_messaging(phone,text):
+   driver.find_element_by_id('com.android.mms:id/action_start_new_conversation').click()
    time.sleep(1)
-   driver.find_element_by_id("com.android.mms:id/send_message_button").click()
+   driver.find_element_by_id('com.android.mms:id/recipient_text_view').send_keys(phone)
+   driver.keyevent(66)
    time.sleep(1)
+   driver.find_element_by_id('com.android.mms:id/compose_message_text').send_keys(text)
+   time.sleep(1)
+   driver.find_element_by_id('com.android.mms:id/send_message_button').click()
+   time.sleep(3)
+   if len(driver.find_elements_by_id("com.android.mms:id/message_text")) > 0:
+      el = driver.find_elements_by_id("com.android.mms:id/message_text")[-1]
+      msg_content = el.find_element_by_id("com.android.mms:id/message_text").get_attribute("text")
+   else:
+      msg_content = "没有发送出去"
+   # 判断是否是最后一条发送的消息
+   if len(driver.find_elements_by_id("com.android.mms:id/message_status")) > 0:
+      el = driver.find_elements_by_id("com.android.mms:id/message_status")[-1]
+      msg_status = el.find_element_by_id("com.android.mms:id/message_status").get_attribute("text")
+   else:
+      msg_status = "没有获取发送状态"
 
 
+
+
+send_messaging('18923810182','dada大文本')
 
 
 
